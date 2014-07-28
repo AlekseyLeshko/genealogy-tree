dd2a04cde240a3a4d6e2de87ac47128b5d2af649
alekseyleshko <alekseyleshko@gmail.com>
2014-07-28 11:16:53 +0400

    Add default options

--- src/js/genealogyTree.js
+++ src/js/genealogyTree.js
@@ -1,2 +1,20 @@
 GenealogyTree = function() {
+  this.options = {};
+};
+
+GenealogyTree.prototype = {
+  createOptions: function(options) {
+    var defaultOptions = this.getDefaultOptions();
+    this.pluginOptions = $.extend(defaultOptions, options);
+  },
+
+  getDefaultOptions: function() {
+    var defaultOptions = {
+      container: {
+        width: 700,
+        height: 700
+      }
+    };
+    return defaultOptions;
+  },
 };
--- test/unit/test.js
+++ test/unit/test.js
@@ -4,5 +4,16 @@ describe("Genealogy tree", function() {
   it("create ", function() {
     var gTree = new GenealogyTree();
     expect(gTree).not.toBeNull();
+    expect(gTree.options).not.toBeUndefined();
+  });
+
+  it("get default options", function() {
+    var defaultWidth = 700;
+    var defaultHeight = 700;
+    var options = GenealogyTree.prototype.getDefaultOptions();
+    expect(options).not.toBeNull();
+    expect(options.container).not.toBeUndefined();
+    expect(options.container.width).toEqual(defaultWidth);
+    expect(options.container.width).toEqual(defaultHeight);
   });
 });

1a298f158b134f2aab55ea9fec5afdb4e87750ef
alekseyleshko <alekseyleshko@gmail.com>
2014-07-31 14:11:19 +0400

    Add jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('lint', function() {
+  return gulp.src('./src/js/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });

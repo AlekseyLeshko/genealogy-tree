1adc72571692fbea300e6980d343758fa7676866
alekseyleshko <alekseyleshko@gmail.com>
2014-07-31 16:42:42 +0400

    Add concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var concat = require('gulp-concat');
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+var paths = {
+  scripts: ['src/js/**/*.js']
+};
 
-gulp.task('jshint', function() {
-  return gulp.src('./src/js/**/*.js')
+gulp.task('scripts', function() {
+  return gulp.src(paths.scripts)
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('dist/js/'));
 });

97ef63e1e841f69ef5b7d1e30f1b584dcddacdcd
alekseyleshko <alekseyleshko@gmail.com>
2014-07-31 16:48:26 +0400

    Add clean

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,13 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require("gulp-rename");
 var sourcemaps = require('gulp-sourcemaps');
+var clean = require('gulp-clean')
 
 var paths = {
   scripts: ['src/js/**/*.js']
 };
 
-gulp.task('scripts', function() {
+gulp.task('scripts', ['clean'], function() {
   return gulp.src(paths.scripts)
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
@@ -20,4 +21,9 @@ gulp.task('scripts', function() {
     .pipe(rename('all.min.map.js'))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('clean', function () {
+  return gulp.src('dist')
+    .pipe(clean());
 });

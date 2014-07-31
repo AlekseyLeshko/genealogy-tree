27857143d7dd065005409add1510c18e6d2fbaac
alekseyleshko <alekseyleshko@gmail.com>
2014-07-31 16:45:15 +0400

    Add rename

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
 
 var paths = {
   scripts: ['src/js/**/*.js']
@@ -14,5 +15,6 @@ gulp.task('scripts', function() {
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(uglify())
     .pipe(concat('all.min.js'))
-    .pipe(gulp.dest('dist/js/'));
+    .pipe(gulp.dest('dist/js/'))
+    .pipe(rename('all.min.map.js'));
 });

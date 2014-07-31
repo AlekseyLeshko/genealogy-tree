75d3caf2b7fb11fccb1c8cb2d8eb44560981a379
alekseyleshko <alekseyleshko@gmail.com>
2014-07-31 14:13:09 +0400

    Add jshint-stylish

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -8,5 +9,5 @@ gulp.task('default', function() {
 gulp.task('lint', function() {
   return gulp.src('./src/js/**/*.js')
     .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    .pipe(jshint.reporter('jshint-stylish'))
 });

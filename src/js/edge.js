a92680ba4d77efa6a7cb118db13493a378394317
alekseyleshko <alekseyleshko@gmail.com>
2014-08-05 16:21:45 +0400

    Update edge class

--- src/js/edge.js
+++ src/js/edge.js
@@ -1,165 +1,154 @@
-var Edge = function(parent, child, type) {
-  //nodes = [], type = ''
+var Edge = (function() {
   'use strict';
+  var Edge = function(parent, child, type) {
+    //nodes = [], type = ''
 
-  this.parent = parent;
-  this.child = child;
-  this.typeRelationship = type;
+    this.parent = parent;
+    this.child = child;
+    this.typeRelationship = type;
 
-  this.init();
-};
+    this.init();
+  };
 
-Edge.prototype = {
-  init: function() {
-    var self = this;
-    this.methodOfType = {
-      'marriage': {
-        calculation: function() {
-          return self.calcCoorTypeMarrige();
+  Edge.prototype = {
+    init: function() {
+      var self = this;
+      this.methodOfType = {
+        'marriage': {
+          calculation: function() {
+            return self.calcCoorTypeMarrige();
+          },
+          render: function() {
+            return self.renderTypeMarrige();
+          }
         },
-        render: function() {
-          return self.renderTypeMarrige();
+        'of_marriage': {
+          calculation: function() {
+            return self.calcCoorTypeOfMarrige();
+          },
+          render: function() {
+            return self.renderTypeOfMarrige();
+          }
         }
-      },
-      'of_marriage': {
-        calculation: function() {
-          return self.calcCoorTypeOfMarrige();
-        },
-        render: function() {
-          return self.renderTypeOfMarrige();
-        }
-      }
-    };
+      };
 
-    this.createOptions();
-  },
+      this.createOptions();
+    },
 
-  createOptions: function(options) {
-    var defaultOptions = {
-      class: 'link',
-      strokeColor: 'black',
-      strokeWidth: 1
-    };
+    createOptions: function(options) {
+      var defaultOptions = {
+        class: 'link',
+        strokeColor: 'black',
+        strokeWidth: 1
+      };
 
-    this.options = $.extend(true, defaultOptions, options);
-  },
+      this.options = $.extend(true, defaultOptions, options);
+    },
 
-  calcCoordinates: function() {
-    this.methodOfType[this.typeRelationship].calculation();
-  },
+    calcCoordinates: function() {
+      this.methodOfType[this.typeRelationship].calculation();
+    },
 
-  calcCoorTypeMarrige: function() {
-    var pairs = [];
-    var dy = 11;
-    var pair = {
-      x: this.parent.x + 14,
-      y: this.parent.y + dy
-    };
-    pairs.push(pair);
-    pair = {
-      x: this.child.x + 5,
-      y: this.child.y + dy
-    };
-    pairs.push(pair);
+    calcCoorTypeMarrige: function() {
+      var pairs = [];
+      var dy = 11;
+      var pair = {
+        x: this.parent.x + 14,
+        y: this.parent.y + dy
+      };
+      pairs.push(pair);
+      pair = {
+        x: this.child.x + 5,
+        y: this.child.y + dy
+      };
+      pairs.push(pair);
 
-    dy = 14;
-    pair = {
-      x: this.parent.x + 14,
-      y: this.parent.y + dy
-    };
-    pairs.push(pair);
-    pair = {
-      x: this.child.x + 5,
-      y: this.child.y + dy
-    };
-    pairs.push(pair);
+      dy = 14;
+      pair = {
+        x: this.parent.x + 14,
+        y: this.parent.y + dy
+      };
+      pairs.push(pair);
+      pair = {
+        x: this.child.x + 5,
+        y: this.child.y + dy
+      };
+      pairs.push(pair);
 
-    var middle = pairs[1].x + ((pairs[0].x - pairs[1].x) / 2);
-    pair = {
-      x: middle,
-      y: this.parent.y + dy
-    };
-    pairs.push(pair);
-    pair = {
-      x: middle,
-      y: this.child.y + dy + 50
-    };
-    pairs.push(pair);
+      var middle = pairs[1].x + ((pairs[0].x - pairs[1].x) / 2);
+      pair = {
+        x: middle,
+        y: this.parent.y + dy
+      };
+      pairs.push(pair);
+      pair = {
+        x: middle,
+        y: this.child.y + dy + 50
+      };
+      pairs.push(pair);
 
-    this.coordinates = {
-      pairs: pairs,
-      middle: middle
-    };
-  },
+      this.coordinates = {
+        pairs: pairs,
+        middle: middle
+      };
+    },
 
-  calcCoorTypeOfMarrige: function() {
-    var pairs = [];
-    var dx = 10;
-    var pair = {
-      x: this.child.x + dx,
-      y: this.child.y + 7
-    };
-    pairs.push(pair);
+    calcCoorTypeOfMarrige: function() {
+      var pairs = [];
+      var dx = 10;
+      var pair = {
+        x: this.child.x + dx,
+        y: this.child.y + 7
+      };
+      pairs.push(pair);
 
-    pair = _.last(this.parent.coordinates.pairs);
-    pairs.push(pair);
+      pair = _.last(this.parent.coordinates.pairs);
+      pairs.push(pair);
 
-    this.coordinates = {
-      pairs: pairs
-    };
-  },
+      this.coordinates = {
+        pairs: pairs
+      };
+    },
 
-  render: function(svgContainer) {
-    this.svgContainer = svgContainer;
-    this.calcCoordinates();
+    render: function(svgContainer) {
+      this.svgContainer = svgContainer;
+      this.calcCoordinates();
 
-    var svgEdge = this.methodOfType[this.typeRelationship].render();
-    return svgEdge;
-  },
+      var svgEdge = this.methodOfType[this.typeRelationship].render();
+      return svgEdge;
+    },
 
-  renderTypeMarrige: function() {
-    var self = this;
-    var container = this.svgContainer
-      .append('g')
-      .attr('class', 'edge')
-      .on('mouseover', function() {
-        self.setColor(this, 'red', 'line');
-      })
-      .on('mouseout', function() {
-        self.setColor(this, 'black', 'line');
-      });
-    this.drawLink(container, this.coordinates.pairs.slice(0, 2));
-    this.drawLink(container, this.coordinates.pairs.slice(2, 4));
-    this.drawLink(container, this.coordinates.pairs.slice(4, 6));
-  },
+    renderTypeMarrige: function() {
+      var self = this;
+      var container = this.svgContainer
+        .append('g')
+        .attr('class', 'edge');
 
-  renderTypeOfMarrige: function() {
-    var self = this;
-    var container = this.svgContainer
-      .append('g')
-      .attr('class', 'edge')
-      .on('mouseover', function() {
-        self.setColor(this, 'red', 'line');
-      })
-      .on('mouseout', function() {
-        self.setColor(this, 'black', 'line');
-      });
+      this.drawLink(container, this.coordinates.pairs.slice(0, 2));
+      this.drawLink(container, this.coordinates.pairs.slice(2, 4));
+      this.drawLink(container, this.coordinates.pairs.slice(4, 6));
+    },
 
-    this.drawLink(container, this.coordinates.pairs.slice(0, 2));
-  },
+    renderTypeOfMarrige: function() {
+      var self = this;
+      var container = this.svgContainer
+        .append('g')
+        .attr('class', 'edge');
 
-  drawLink: function(container, pairs) {
-    container
-      .append('line')
-      .attr('x1', pairs[0].x)
-      .attr('y1', pairs[0].y)
-      .attr('x2', pairs[1].x)
-      .attr('y2', pairs[1].y)
-      .attr('stroke-width', this.options.strokeWidth)
-      .attr('stroke', this.options.strokeColor);
-  },
+      this.drawLink(container, this.coordinates.pairs.slice(0, 2));
+    },
 
-  setColor: function(el, color, type) {
-    d3.select(el).selectAll(type).style('stroke', color);
-  }
-};
+    drawLink: function(container, pairs) {
+      container
+        .append('line')
+        .attr('x1', pairs[0].x)
+        .attr('y1', pairs[0].y)
+        .attr('x2', pairs[1].x)
+        .attr('y2', pairs[1].y)
+        .attr('stroke-width', this.options.strokeWidth)
+        .attr('stroke', this.options.strokeColor);
+    }
+  };
+
+  return Edge;
+})();
